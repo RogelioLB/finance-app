2ddacc00ead0e3978fbdcd2d5af58929774f5978
RogelioLB <rogelio20052011@gmail.com>
2025-12-14 20:56:18 -0700

    Deploy

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+// Define las rutas que quieres proteger
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-    if (isProtectedRoute(req)) await auth.protect();
+    // Si la ruta es protegida, forzamos la protección
+    if (isProtectedRoute(req)) {
+        await auth.protect();
+    }
 });
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: [
+        // Excluye archivos estáticos y de Next.js
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Siempre ejecuta para rutas API
+        '/(api|trpc)(.*)',
+    ],
 };
